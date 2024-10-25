926b4eed927ef7483494f375c3799743f5daf862
Garth Scaysbrook <brooksy4503@gmail.com>
2024-10-25 18:19:24 +1000

    added netlify.toml and fixed next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // your config options here
+  output: "standalone",
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
